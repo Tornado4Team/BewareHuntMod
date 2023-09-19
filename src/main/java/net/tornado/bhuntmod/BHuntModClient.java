52485d6370c5b250adf92ecc9ba7c36a4367bab9
Tornado <tornado.zajdel@gmail.com>
2023-09-19 13:37:18 +0200

    Initial commit

---
+++ src/main/java/net/tornado/bhuntmod/BHuntModClient.java
@@ -0,0 +1,11 @@
+package net.tornado.bhuntmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BHuntModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/tornado/bhuntmod/BHuntModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.tornado.bhuntmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BHuntModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
